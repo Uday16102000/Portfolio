26d734ff84b6ba6577a2c2acf6c99e4e88d81e45
UdayMoreV2S <uday.more@v2stech.com>
2024-03-10 12:59:55 +0530

    logo color change

--- UdayPortfolio/src/app/header/header.component.ts
+++ UdayPortfolio/src/app/header/header.component.ts
@@ -13,15 +13,5 @@ addToggle()
 {
   this.status = !this.status;       
 }
-handleScrollStart() {
-  console.log('scroll start');
-}
 
-handleScroll(ev: CustomEvent<ScrollDetail>) {
-  console.log('scroll', ev.detail);
-}
-
-handleScrollEnd() {
-  console.log('scroll end');
-}
 }
